ba1335e0de0abaef43a6647c1afe41dadaeecfa0
rene.lopez <rene.lopez.cano@gmail.com>
2015-09-01 18:11:11 -0500

    Fourth Commit

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -8,4 +8,21 @@ module.exports=function(config){
     db.once('open', function callback() {
         console.log('multivision db opened');
     });
+
+
+    var userSchema=mongoose.Schema({
+        firstName:String,
+        lastName:String,
+        username:String
+    });
+
+    var User=mongoose.model('User',userSchema);
+
+    User.find({}).exec(function(err,collection){
+        if(collection.length===0){
+            User.create({firstName:"Joe",lastName:'Eames',username:'joe'});
+            User.create({firstName:"John",lastName:'Papa',username:'john'});
+            User.create({firstName:"Dan",lastName:'Whalin',username:'dan'});
+        }
+    });
 };
