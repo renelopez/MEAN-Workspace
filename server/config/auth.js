e73c40a6230305cd88404f77909ebd87afbf6518
rene.lopez <rene.lopez.cano@gmail.com>
2015-09-04 19:11:09 -0500

    Seventh Commit

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,3 +1,21 @@
-/**
- * Created by rene.lopez on 9/4/2015.
- */
+var passport=require('passport');
+
+
+exports.authenticate= function (req, res, next) {
+    var auth = passport.authenticate('local', function (err, user) {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            res.send({success: false})
+        }
+        req.logIn(user, function (err) {
+            if (err) {
+                return next(err);
+            }
+            res.send({success: true, user: user})
+        });
+    });
+    auth(req, res, next);
+};
+
